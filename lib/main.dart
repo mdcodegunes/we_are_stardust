73252e88f6708cd247e078e77ada66d36a030160
Ccode Gunes <mdcodegunes@gmail.com>
2022-07-27 19:43:25 +0300

    First commit Message

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//The main function is the starting point for all our Flutter apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Center(child: Text('We are Stardust002')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(image: AssetImage('images/nebula.webp')),
+        ),
+      ),
+    ),
+  );
+}
